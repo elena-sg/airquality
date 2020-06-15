006f1102737177f469609d607ae5f6c88dff1c5f
Elena Sobrini García <elenasobrini96@gmail.com>
2020-06-15 10:16:03 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Evolution of the air quality over time. How is it related to human activity?',
+    author='elena-sg',
+    license='',
+)
